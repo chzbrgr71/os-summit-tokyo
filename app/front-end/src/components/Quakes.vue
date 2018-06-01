c79220ecb82a3f739ab16bc35717358a0af5f7df
Joey Schluchter <jschluchter@gmail.com>
2018-06-01 13:13:47 -0400

    updated styling

--- app/front-end/src/components/Quakes.vue
+++ app/front-end/src/components/Quakes.vue
@@ -25,12 +25,12 @@ export default {
   created() {},
   computed: {},
   mounted() {
-    vm = this;
+    vm = this
     mapboxgl.accessToken =
       'pk.eyJ1Ijoic29ub2pvcmVsIiwiYSI6ImNqaDl1Z25udzAybGMzNnBmbzl4NDBsam0ifQ.itgTNw7IhsoPTwkxiPz7Vw';
     map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/sonojorel/cjhqx01wc6cqr2rmp9b5x70w4',
+      style: 'mapbox://styles/sonojorel/cjhw8422i15g72snx2zsof3s1',
       center: [-79.995888, 40.440624],
       zoom: 2
     })
@@ -39,110 +39,128 @@ export default {
       vm.loadQuakes()
     })
 
-    map.on('click', 'points', function (e) {
-      //console.log(e.features[0].properties.mag);
-        var coordinates = e.features[0].geometry.coordinates.slice();
-        var detail = e.features[0].properties;
-        var header = '<h3>Magnitude: ' + detail.mag + '</h3><ul>';
-        var eventTime = new Date(detail.time);
-        var place = '<li><strong>Location:</strong>&nbsp;' + detail.place + '</li>';
-        var dated = '<li><strong>Date/Time:</strong>&nbsp;' + eventTime + '</li>'; 
-        var end = '<ul>';
-        var html = header.concat(place, dated, end);
-        //  url
-        // title
-// <i class="fas fa-camera-retro"></i>
+    //** show current map center lat lng on page **//
+    map.on('moveend', function(e) {
+      vm.latitude = map.getCenter().lat
+      vm.longitude = map.getCenter().lng
+    })
 
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
+  },
+  methods: {
+    filterItems(type, data, cb){
+      var output
+      switch(type) {
+          case 'yellow':
+              output = data.filter(quake => quake.properties.mag < 4.8)
+              cb(output)
+              break
+          case 'orange':
+              output = data.filter(quake => (quake.properties.mag > 4.8 && quake.properties.mag <5.2))
+              cb(output)
+              break
+          case 'red':
+              output = data.filter(quake => quake.properties.mag >= 5.2)
+              cb(output)
+              break
+          default:
+              break
+      }
+
+    },
+    addLayer(type, data){
+      var icon = 'epi-' + type
+      var size
+
+      switch(type){
+        case 'yellow':
+          size = .3
+          break
+        case 'orange':
+          size = .35
+          break
+        case 'red':
+          size = .4
+          break
+        default:
+          break
+      }
+      
+      map.addLayer(
+          {
+            'id': type,
+            'type': 'symbol',
+            'source': {
+              'type': 'geojson',
+              'data': {
+                  'type': 'FeatureCollection',
+                  'features': data
+              }
+            },
+            'layout': {
+              'icon-image': icon,
+              'icon-size': size,
+              //'text-field': '{mag}',
+              'icon-allow-overlap': true,
+              'text-allow-overlap':true,
+              // 'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+              //'text-size': 10,
+              //'text-offset': [0, 0.6],
+              //'text-anchor': 'top'
+            },
+            'paint':{
+              // 'text-color':'#555',
+              // 'text-halo-color':'rgba(255, 255, 255, .50)',
+              // 'text-halo-width':1
+            }
+            })
+
+      map.on('click', type, function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice()
+        var detail = e.features[0].properties
+        var header = '<h3>Magnitude: ' + detail.mag + '</h3><ul>'
+        var eventTime = new Date(detail.time)
+        var place = '<li><strong>Location:</strong>&nbsp;' + detail.place + '</li>'
+        var dated = '<li><strong>Date/Time:</strong>&nbsp;' + eventTime + '</li>'
+        var end = '<ul>'
+        var html = header.concat(place, dated, end)
+
         while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
         }
 
         new mapboxgl.Popup()
             .setLngLat(coordinates)
             .setHTML(html)
-            .addTo(map);
-    });
-
-    //** show current map center lat lng on page **//
-    map.on('moveend', function(e) {
-      vm.latitude = map.getCenter().lat
-      vm.longitude = map.getCenter().lng
-      // console.log(map.getCenter().lng)
-      // console.log(map.getCenter().lat)
+            .addTo(map)
     })
 
-    // map.on('mousemove', (e) => {
-    //     console.log(e.lngLat)
-    // })
-  },
-  methods: {
+    },
     loadQuakes() {
+
+
       let payload
-      const myRequest = new Request('http://104.43.132.254:8080/api/earthquakes')
+      const myRequest = new Request('http://104.43.128.138:8080/api/earthquakes')
       fetch(myRequest)
-      .then((response) => { return response.json() })
+      .then((response) => { 
+        return response.json() })
       .then((data) => {
         payload = data.payload
-        map.addLayer(
-          {
-          'id': 'points',
-          'type': 'symbol',
-          'source': {
-              'type': 'geojson',
-              'data': {
-                  'type': 'FeatureCollection',
-                  'features': data.payload
-              }
-          },
-          'layout': {
-              'icon-image': 'circle-11',
-              'text-field': '{mag}',
-              'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-              'text-offset': [0, 0.6],
-              'text-anchor': 'top'
-          },'paint': {
-              'icon-color':'#FFCC00',
-          }
-                })
-         })
-         .catch(error => { console.log(error) })
+
+        vm.filterItems('red', payload, function(red){
+          console.log(red.length)
+          vm.addLayer('red', red)
+        })
+        vm.filterItems('orange', payload, function(orange){
+          console.log(orange.length)
+          vm.addLayer('orange', orange)
+        })
+        vm.filterItems('yellow', payload, function(yellow){
+          console.log(yellow.length)
+          vm.addLayer('yellow', yellow)
+        })
+      })
+       
     }
-    // showFortress() {
-    //   //console.log(this.map);
-    //   map.loadImage('http://localhost:8080/static/super.png', function(
-    //     error,
-    //     image
-    //   ) {
-    //     if (error) throw error;
-    //     map.addImage('super', image);
-    //     map.addLayer({
-    //       id: 'points',
-    //       type: 'symbol',
-    //       source: {
-    //         type: 'geojson',
-    //         data: {
-    //           type: 'FeatureCollection',
-    //           features: [
-    //             {
-    //               type: 'Feature',
-    //               geometry: {
-    //                 type: 'Point',
-    //                 coordinates: [-82.8062, 27.9359]
-    //               }
-    //             }
-    //           ]
-    //         }
-    //       },
-    //       layout: {
-    //         'icon-image': 'super',
-    //         'icon-size': 2
-    //       }
-    //     })
-    //   })
-    // }
   }
 }
 </script>
