bf0f30361b5033e0919474642284598ebb353eaf
Brian Redmond <brianisrunning@gmail.com>
2018-06-06 09:09:59 -0600

    added routerule

--- brigade.js
+++ brigade.js
@@ -16,36 +16,41 @@ events.on("push", (brigadeEvent, project) => {
     var imageTag = branch + "-" + gitSHA
     var acrImage = image + ":" + imageTag
 
-    console.log(`==> gitHub webook (${branch}) with commit ID ${gitSHA}`)
-
-    // setup container build brigade job
-    var acr = new Job("job-runner-acr-builder")
-    acr.storage.enabled = false
-    acr.image = "microsoft/azure-cli:2.0.32"
-    acr.tasks = [
-        `cd /src/app/api-location`,
-        `az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
-        `az acr build -t ${acrImage} -r ${acrName} .`
-    ]
-
-    // brigade job. Helm chart
-    var helm = new Job("job-runner-helm")
-    helm.storage.enabled = false
-    helm.image = "chzbrgr71/k8s-helm:v2.9.1"
-    var imageFull = acrServer + "/" + image
-    helm.tasks = [
-        `helm upgrade --install api-location ./src/charts/api-location --namespace quake --set location.image=${imageFull} --set location.imageTag=${imageTag} --set location.deployment=api-location --set location.versionLabel=prod`
-    ]
-
-    var pipeline = new Group()
-    pipeline.add(acr)
-    pipeline.add(helm)
     
+
     if (branch == "master") {
+        console.log(`==> gitHub webook (${branch}) with commit ID ${gitSHA}`)
+        
+        // setup container build brigade job
+        var acr = new Job("job-runner-acr-builder")
+        acr.storage.enabled = false
+        acr.image = "microsoft/azure-cli:2.0.32"
+        acr.tasks = [
+            `cd /src/app/api-location`,
+            `az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
+            `az acr build -t ${acrImage} -r ${acrName} .`
+        ]
+
+        // brigade job. Helm chart
+        var helm = new Job("job-runner-helm")
+        helm.storage.enabled = false
+        helm.image = "chzbrgr71/k8s-helm:v2.9.1"
+        var imageFull = acrServer + "/" + image
+        helm.tasks = [
+            `helm upgrade --install api-location ./src/charts/api-location --namespace quake --set location.image=${imageFull} --set location.imageTag=${imageTag} --set location.deployment=api-location --set location.versionLabel=prod`,
+            `helm upgrade --install routes ./src/charts/routes --set prodWeight=50 --set canaryWeight=50`
+        ]
+
+        var pipeline = new Group()
+        pipeline.add(acr)
+        pipeline.add(helm)
         pipeline.runEach()
+    } else if (branch == "dev") {
+        console.log(`==> gitHub webook (${branch}) with commit ID ${gitSHA}`)
+        
     } else {
-        console.log(`==> no jobs to run when not master`)
-    } 
+        console.log(`==> gitHub webook (${branch}) with commit ID ${gitSHA}`)
+    }
 
 })
 
