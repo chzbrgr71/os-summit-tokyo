e6e6ba417703e193eb4f4bb9a4059a8c2a613ff7
Brian Redmond <brianisrunning@gmail.com>
2018-06-06 10:24:30 -0600

    added code for dev branch

--- brigade.js
+++ brigade.js
@@ -15,27 +15,25 @@ events.on("push", (brigadeEvent, project) => {
     var branch = getBranch(gitPayload)
     var imageTag = branch + "-" + gitSHA
     var acrImage = image + ":" + imageTag
+    var imageFull = acrServer + "/" + image
 
-    
+    // setup container build brigade job
+    var acr = new Job("job-runner-acr-builder")
+    acr.storage.enabled = false
+    acr.image = "microsoft/azure-cli:2.0.32"
+    acr.tasks = [
+        `cd /src/app/api-location`,
+        `az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
+        `az acr build -t ${acrImage} -r ${acrName} .`
+    ]
 
     if (branch == "master") {
-        console.log(`==> gitHub webook (${branch}) with commit ID ${gitSHA}`)
+        console.log(`==> gitHub webook on master branch with commit ID ${gitSHA}`)
         
-        // setup container build brigade job
-        var acr = new Job("job-runner-acr-builder")
-        acr.storage.enabled = false
-        acr.image = "microsoft/azure-cli:2.0.32"
-        acr.tasks = [
-            `cd /src/app/api-location`,
-            `az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
-            `az acr build -t ${acrImage} -r ${acrName} .`
-        ]
-
         // brigade job. Helm chart
         var helm = new Job("job-runner-helm")
         helm.storage.enabled = false
         helm.image = "chzbrgr71/k8s-helm:v2.9.1"
-        var imageFull = acrServer + "/" + image
         helm.tasks = [
             `helm upgrade --install api-location ./src/charts/api-location --namespace quake --set location.image=${imageFull} --set location.imageTag=${imageTag} --set location.deployment=api-location --set location.versionLabel=prod`,
             `helm upgrade --install routes ./src/charts/routes --namespace quake --set prodWeight=100 --set canaryWeight=0`
@@ -45,11 +43,26 @@ events.on("push", (brigadeEvent, project) => {
         pipeline.add(acr)
         pipeline.add(helm)
         pipeline.runEach()
+
     } else if (branch == "dev") {
-        console.log(`==> gitHub webook (${branch}) with commit ID ${gitSHA}`)
+        console.log(`==> gitHub webook on dev branch with commit ID ${gitSHA}`)
         
+        // brigade job. Helm chart
+        var helm = new Job("job-runner-helm")
+        helm.storage.enabled = false
+        helm.image = "chzbrgr71/k8s-helm:v2.9.1"
+        helm.tasks = [
+            `helm upgrade --install api-location-canary ./src/charts/api-location --namespace quake --set location.image=${imageFull} --set location.imageTag=${imageTag} --set location.deployment=api-location-canary --set location.versionLabel=canary`,
+            `helm upgrade --install routes ./src/charts/routes --namespace quake --set prodWeight=50 --set canaryWeight=50`
+        ]
+
+        var pipeline = new Group()
+        pipeline.add(acr)
+        pipeline.add(helm)
+        pipeline.runEach()
+
     } else {
-        console.log(`==> gitHub webook (${branch}) with commit ID ${gitSHA}`)
+        console.log(`==> no action on (${branch}) with commit ID ${gitSHA}`)
     }
 
 })
